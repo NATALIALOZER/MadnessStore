ae430d111d491a20b218794cd355306022466e89
NATALIALOZER <badwolf101115@gmail.com>
2021-07-15 15:18:58 +0300

    added @media for devices

--- app/js/main.js
+++ app/js/main.js
@@ -10,8 +10,15 @@ $(function () {
     centerMode: true,
     variableWidth: true,
   });
-
 });
+
+
+
+$(".menu__item" ).click(function() {
+	$("#menu__toggle").prop("checked", false);
+});
+
+
 
 
 $(document).ready(function() {
@@ -55,67 +62,3 @@ $(document).ready(function() {
 
 
 
-
-
-
-
-
-// $(document).ready(function () {
-//   //E-mail Ajax Send
-//   $(".footer__form").submit(function () {
-//     //Change
-//     var th = $(this);
-//     $.ajax({
-//       type: "POST",
-//       url: "mail.php", //Change
-//       data: th.serialize(),
-//     }).done(function () {
-//       alert("Thank you!");
-//       setTimeout(function () {
-//         // Done Functions
-//         th.trigger("reset");
-//       }, 1000);
-//     });
-//     return false;
-//   });
-// });
-
-// "use strict"
-
-// document.addEventListener('DOMContentLoaded',function(){
-//     const form = document.getElementById('form');
-//     form.addEventListener('submit', formSend);
-
-//     async function formSend(e){
-//         e.preventDefault();
-//         let error =formValidate(form);
-//     }
-
-//     function formValidate(form){
-//         let error = 0;
-//         let formReq = document.querySelectorAll('._req')
-
-//         for(let index = 0; index<formReq.length; index++){
-//             const input = formReq[index];
-//             formRemoveError(input);
-
-//             if(input.classList.contains('_email')){
-
-//             }
-
-//         }
-//     }
-
-//     function formAddError(input){
-//         input.parentElement.classList.add('_error')
-//         input.classList.add('_error')
-//     }
-//     function formRemoveError(input){
-//         input.parentElement.classList.remove('_error')
-//         input.classList.remove('_error')
-//     }
-
-//     function emailTest(input) {
-//         return
-//     }
-// });
